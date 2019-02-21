017e221dbe709ae4ca3154709de432556a95e6b8
JohnnyC <j0hnnywchiang@gmail.com>
2019-02-21 12:40:18 -0800

    reduxextension

--- src/config/store.js
+++ src/config/store.js
@@ -1,1 +1,11 @@
-import { createStore } from "redux";
+import { createStore, combineReducers } from "redux";
+import playerReducer from "../features/player";
+
+const rootReducer = combineReducers({
+  player: playerReducer
+});
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
