2683ec9db5492720e741f649fd02f9056d8eb453
JohnnyC <j0hnnywchiang@gmail.com>
2019-02-23 12:13:05 -0800

    map component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import Map from "./features/map";
 import Player from "./features/player";
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Map />
         <Player />
       </div>
     );
