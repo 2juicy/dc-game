7ac1e6d63a63c407e8e799c1016119a703b5f7b0
JohnnyC <j0hnnywchiang@gmail.com>
2019-03-21 09:25:55 -0700

    lowered height constraint

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,3 @@
 export const SPRITE_SIZE = 30;
-export const MAP_HEIGHT = SPRITE_SIZE * 13;
-export const MAP_WIDTH = SPRITE_SIZE * 25;
+export const MAP_HEIGHT = SPRITE_SIZE * 12;
+export const MAP_WIDTH = SPRITE_SIZE * 26;
