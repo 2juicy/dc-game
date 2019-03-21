c1eac7d5bd24bc13577a29d041c58c4206369fd8
JohnnyC <j0hnnywchiang@gmail.com>
2019-03-21 14:06:26 -0700

    Updates

---
+++ precache-manifest.e7034cab8268c1e1b935fabb6586c5ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f19440f74f1c8126fdcf003e3d024da",
+    "url": "/dc-game/static/media/player_walk.9f19440f.png"
+  },
+  {
+    "revision": "c75294a3872a8d7e0983",
+    "url": "/dc-game/static/js/runtime~main.c75294a3.js"
+  },
+  {
+    "revision": "81d27a3dbcb1676de97c",
+    "url": "/dc-game/static/js/main.81d27a3d.chunk.js"
+  },
+  {
+    "revision": "5f0cd0489c779fd96663",
+    "url": "/dc-game/static/js/2.5f0cd048.chunk.js"
+  },
+  {
+    "revision": "81d27a3dbcb1676de97c",
+    "url": "/dc-game/static/css/main.38880e47.chunk.css"
+  },
+  {
+    "revision": "86954ddac3b1c8789ed335dda61365cf",
+    "url": "/dc-game/index.html"
+  }
+];
