f449602852f5b5548c233610cda32489ac5889b8
JohnnyC <j0hnnywchiang@gmail.com>
2019-03-21 14:14:18 -0700

    redeploy

---
+++ src/features/map/index.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { SPRITE_SIZE } from "../../config/constants";
+import "./style.css";
+
+function getTileSprite(type) {
+  switch (type) {
+    case 0:
+      return "floor";
+    case 5:
+      return "rock";
+    case 6:
+      return "tree";
+    default:
+      return "floor";
+  }
+}
+
+function MapTile(props) {
+  return (
+    <div
+      className={`tile ${getTileSprite(props.tile)}`}
+      style={{
+        height: SPRITE_SIZE,
+        width: SPRITE_SIZE
+      }}
+    />
+  );
+}
+
+function MapRow(props) {
+  return (
+    <div
+      className="row"
+      style={{
+        height: SPRITE_SIZE
+      }}
+    >
+      {props.tiles.map(tile => (
+        <MapTile tile={tile} />
+      ))}
+    </div>
+  );
+}
+
+function Map(props, key) {
+  return (
+    <div
+      style={{
+        position: "relative",
+        top: "0px",
+        left: "0px",
+        width: "800px",
+        height: "400px",
+        border: "2.5px solid black"
+      }}
+    >
+      {props.tiles.map(row => (
+        <MapRow tiles={row} />
+      ))}
+    </div>
+  );
+}
+
+export default Map;
