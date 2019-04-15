ed2ce5da68c955e3476cc9b52afab156dec0d4ef
JohnnyC <j0hnnywchiang@gmail.com>
2019-04-15 02:43:36 -0700

    combat modal start

--- src/features/combat/combat.js
+++ src/features/combat/combat.js
@@ -1,2 +1,24 @@
 import React from "react";
 import { connect } from "react-redux";
+import { range, sample } from "../../array";
+import combat from ".";
+
+function combatModal(props) {
+  return (
+    <div>
+      <div className="enemy">
+        {/* add image of enemy */}
+        <img src={`monsters/${props.enemy.image}`} />
+      </div>
+      <div className="menu" />
+    </div>
+  );
+}
+
+function mapStateToProps(state) {
+  return {
+    map: state.map
+  };
+}
+
+export default connect(mapStateToProps)(combatModal);
