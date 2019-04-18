fee28fcca68d226b2d9eb0db71914b1308cc0e18
JohnnyC <j0hnnywchiang@gmail.com>
2019-04-18 12:57:36 -0700

    prob have to use redux

--- src/features/modal/index.js
+++ src/features/modal/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import Combat from "../../features/combat";
 import store from "../../config/store";
@@ -14,31 +14,35 @@ function randomEnemy(enemies, levelRange) {
   };
 }
 
-const handleCombatKeys = e => {
-  if (store.getState().modal.visible) {
-    e.preventDefault();
-    switch (e.keyCode) {
-      case 37:
-        return console.log("WEST");
-      case 38:
-        return console.log("NORTH");
-      case 39:
-        return console.log("EAST");
-      case 40:
-        return console.log("SOUTH");
-      case 32:
-        return store.dispatch({ type: "END_COMBAT" });
-      default:
-        console.log(e.keyCode);
-    }
-  }
-};
-
 function Modal(props) {
-  window.addEventListener("keydown", handleCombatKeys);
+  useEffect(() => {
+    window.addEventListener("keydown", handleCombatKeys);
+    return () => {
+      window.removeEventListener("keydown", handleCombatKeys);
+    };
+  }, []);
+
+  const handleCombatKeys = e => {
+    if (store.getState().modal.visible) {
+      e.preventDefault();
+      switch (e.keyCode) {
+        case 37:
+          return console.log("WEST");
+        case 38:
+          return console.log("NORTH");
+        case 39:
+          return console.log("EAST");
+        case 40:
+          return console.log("SOUTH");
+        case 32:
+          return store.dispatch({ type: "END_COMBAT" });
+        default:
+          console.log(e.keyCode);
+      }
+    }
+  };
 
   const enemy = randomEnemy(map.enemies, map.levelRange);
-  console.log(enemy);
 
   return (
     <div
